575f1f2855f90a604ebd528b1d1ca4f9085c2278
artemdljnk <artemodessa31.01@gmail.com>
2025-05-03 12:15:54 +0300

    hw1

---
+++ 1/main.js
@@ -0,0 +1,45 @@
+// -------------------#dYQNrBV---------------------
+
+// let txt1 = 'hello'
+// let txt2 = 'owu'
+// let txt3 = 'com'
+// let txt4 = 'ua'
+// let num1 = 1
+// let num2 = 10
+// let num3 = -999
+// let num4 = 123
+// let num5 = 3.14
+// let num6 = 2.7
+// let num7 = 16
+// let boolean1 = true
+// let boolean2 = false
+//
+// console.log(txt1, txt2, txt3, txt4, num1, num2, num3, num4, num5, num6, num7, boolean1, boolean2)
+
+// -------------------#6Qb97gsv-------------------
+
+// let firstName = 'John';
+// let lastName = 'Doe';
+// let middleName = 'Hores';
+//
+// let person = `${firstName} ${lastName} ${middleName}`;
+// console.log(person);
+
+// ----------------#4N0y5tufA-------------------------
+
+// let a = 100;
+// let b = `100`;
+// let c = true;
+//
+// console.log(typeof a, a)
+// console.log(typeof b, b)
+// console.log(typeof c, c)
+
+// --------------#ruUtWDUI------------------------
+
+// let firstName = prompt("What is your firstName?");
+// let lastName = prompt("What is your lastName?");
+// let age = +prompt("What is your age?");
+//
+// let person = `${firstName} ${lastName} ${age}`;
+// console.log(person);
