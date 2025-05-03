b39facfb13ed6eda7ed7cb8e119ebcb3bf8a2a2e
artemdljnk <artemodessa31.01@gmail.com>
2025-05-03 12:39:15 +0300

    hw2

---
+++ 2/main.js
@@ -0,0 +1,108 @@
+// -----------------------#67kfznmiMl---------------------
+
+// let array = ['hello', 'okten', 'com', 'ua', 10, 3.14, -999, 1, true, false]
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[2])
+// console.log(array[3])
+// console.log(array[4])
+// console.log(array[5])
+// console.log(array[6])
+// console.log(array[7])
+// console.log(array[8])
+// console.log(array[9])
+
+// ---------------------#LARqoUj5I----------------
+
+// let book1 = {
+//     title: "Adam",
+//     pageCount: 1,
+//     genre: "Adam",
+// }
+// let book2 = {
+//     title: "John",
+//     pageCount: 5,
+//     genre: "John",
+// }
+// let book3 = {
+//     title: "Mike",
+//     pageCount: 30,
+//     genre: "Mike",
+// }
+
+// ---------------------#sA3Gg1sCp------------
+
+// let book1 = {
+//     title: "Adam",
+//     pageCount: 1,
+//     genre: "Adam",
+//     author: [
+//         {name: 'Adam', age: 20},
+//         {name: 'Adam', age: 20},
+//     ]
+// }
+// let book2 = {
+//     title: "John",
+//     pageCount: 5,
+//     genre: "John",
+//     author: [
+//         {name: 'John', age: 20},
+//         {name: 'John', age: 20},
+//     ]
+// }
+// let book3 = {
+//     title: "Mike",
+//     pageCount: 30,
+//     genre: "Mike",
+//     author: [
+//         {name: 'Mike', age: 20},
+//         {name: 'Mike', age: 20},
+//     ]
+// }
+
+// ----------------------------#jCHFnEbdmFd----------------
+
+// let array = [
+//     {name: 'John', username: 'John', password: '123456'},
+//     {name: 'John', username: 'John', password: '123456'},
+//     {name: 'John', username: 'John', password: '123456'},
+//     {name: 'John', username: 'John', password: '123456'},
+//     {name: 'John', username: 'John', password: '123456'},
+//     {name: 'John', username: 'John', password: '123456'},
+//     {name: 'John', username: 'John', password: '123456'},
+//     {name: 'John', username: 'John', password: '123456'},
+//     {name: 'John', username: 'John', password: '123456'},
+//     {name: 'John', username: 'John', password: '123456'},
+// ]
+// console.log(array[0].password)
+// console.log(array[1].password)
+// console.log(array[2].password)
+// console.log(array[3].password)
+// console.log(array[4].password)
+// console.log(array[5].password)
+// console.log(array[6].password)
+// console.log(array[7].password)
+// console.log(array[8].password)
+// console.log(array[9].password)
+
+// ------------#coYydZuaeEB-------------
+
+// let temp = [
+//     {morning: 13, day: 23, night: -1},
+//     {morning: 13, day: 5, night: 1},
+//     {morning: 13, day: 1, night: 1},
+//     {morning: 33, day: 11, night: 1},
+//     {morning: 13, day: 31, night: 1},
+//     {morning: 43, day: 21, night: 1},
+//     {morning: 13, day: 11, night: 1},
+// ]
+
+// ---------------------#bAUsaq6LI-------------------
+
+// let x = +prompt('Enter your number: ')
+//
+// if (x !== 0){
+//     console.log('Yes')
+// }else{
+//     console.log('No')
+// }
