42f0bb9665f70af28a351679797591ba0d0eeb3c
artemdljnk <artemodessa31.01@gmail.com>
2025-05-04 15:30:40 +0300

    hw4

---
+++ 4/main.js
@@ -0,0 +1,31 @@
+// ----------------------#I2XsG6f---------------
+
+// function solve(a, b) {
+//     let num = a * b
+//     return num
+// }
+// console.log(solve(10, 11))
+
+// --------------#ETGAxbEn8l-------------
+
+// function solve(r) {
+//     let result = Math.PI * r * r;
+//     return result;
+// }
+// console.log(solve(2, 3));
+
+// ------------#Mbiz5K4yFe7---------------
+
+// function solve(r, h) {
+//     return 2 * Math.PI * r * h
+// }
+//
+// console.log(solve(2, 2))
+
+// ------------------#SIdMd0hQ------------
+
+// function solve(array) {
+//     for (const arrayElement of array) {
+//         console.log(arrayElement)
+//     }
+// }
