20f8ed94a66011541e396899b45e49b1fa4d81b9
artemdljnk <artemodessa31.01@gmail.com>
2025-05-07 15:40:29 +0300

    hw4

---
+++ 4/main.js
@@ -0,0 +1,54 @@
+// ----------------------#I2XsG6f---------------
+
+// function solve(a, b) {
+//     let result = a * b
+//     return result
+// }
+
+// --------------#ETGAxbEn8l-------------
+
+// function solve(r) {
+//     return Math.PI * r * r
+// }
+
+// ------------#Mbiz5K4yFe7---------------
+
+// function solve(radius, height) {
+//     return 2 * Math.PI * radius * height;
+// }
+
+// ------------------#SIdMd0hQ------------
+
+// function solve(array) {
+//     for (const arrayElement of array) {
+//         console.log(arrayElement);
+//     }
+// }
+
+// ---------------#59g0IsA------------
+
+// function solve(text) {
+//     document.write(`<p>${text}</p>`);
+// }
+
+// ---------------#hOL6126---------
+
+// function solve(text) {
+//     document.write(`
+//     <ul>
+//         <li>${text}</li>
+//         <li>${text}</li>
+//         <li>${text}</li>
+//     </ul>
+//     `)
+// }
+
+// -------------#0Kxco1edSN--------------
+
+// function solve(text, counter) {
+//     document.write('<ul>');
+//     for (let i = 0; i < counter; i++) {
+//         document.write(`'<li>' + ${text} + '</li>'`);
+//     }
+//     document.write('</ul>');
+// }
