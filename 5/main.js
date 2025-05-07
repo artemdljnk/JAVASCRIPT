e622e0f323f730cd171c5189b4e90aeaf14ca5f4
artemdljnk <artemodessa31.01@gmail.com>
2025-05-07 15:46:21 +0300

    hw5

---
+++ 5/main.js
@@ -0,0 +1,45 @@
+// let solve = (a, b) => a + b;
+
+// -------------------------
+
+// let solve = (r) => Math.PI * r * r;
+
+// --------------
+
+// let solve = (radius, height) => 2 * Math.PI * radius * height;
+
+// ----------------
+
+// let solve = (array) => {
+//     for (const arrayElement of array) {
+//         console.log(arrayElement)
+//     }
+// }
+
+// ----------------------------
+
+// let solve = (text) => {
+//     document.write(`<p>${text}</p>`);
+// }
+
+// --------------------------
+
+// let solve = (text) => {
+//         document.write(`
+//     <ul>
+//         <li>${text}</li>
+//         <li>${text}</li>
+//         <li>${text}</li>
+//     </ul>
+//     `)
+// }
+
+// ----------------------
+
+// let solve = (text, counter) => {
+//         document.write('<ul>');
+//     for (let i = 0; i < counter; i++) {
+//         document.write(`'<li>' + ${text} + '</li>'`);
+//     }
+//     document.write('</ul>');
+// }
