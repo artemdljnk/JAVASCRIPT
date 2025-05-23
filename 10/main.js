2feb06df16f49e8cdf6be0bcd4c16c5a9381dde8
ArtemDljnk <artemodessa31.01@gmail.com>
2025-05-23 16:57:33 +0300

    hw10

---
+++ 10/main.js
@@ -0,0 +1,17 @@
+// ----------------#sH8c4er-----------
+
+// let button = document.getElementById("btn")
+// button.onclick = function () {
+//     document.getElementById("text").remove()
+// }
+
+// -------------#ymAmN2xJ------------
+
+let someForm = document.forms.someForm
+
+const buttonValue = someForm.send-button;
+send-button.addEventListener('click', () => {
+    const nameValue = someForm.name.value;
+    const surnameValue = someForm.surname.value;
+    const ageValue = someForm.surname.age;
+})
