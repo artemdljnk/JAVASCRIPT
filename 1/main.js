70cefd218dffeba4354671d7b0689114c47dcbcc
ArtemDljnk <artemodessa31.01@gmail.com>
2025-08-11 12:03:56 +0300

    hw1

---
+++ 1/main.js
@@ -0,0 +1,55 @@
+// --------------------#dYQNrBV---------------
+
+// let text1 = 'hello'
+// let text2 = 'owu'
+// let text3 = 'com'
+// let text4 = 'ua'
+// let num1 = 1
+// let num2 = 10
+// let num3 = -999
+// let num4 = 123
+// let num5 = 3.14
+// let num6 = 2.7
+// let num7 = 16
+// let boolean1 = true
+// let boolean2 = false
+//
+// console.log(text1)
+// console.log(text2)
+// console.log(text3)
+// console.log(text4)
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+// console.log(num4)
+// console.log(num5)
+// console.log(num6)
+// console.log(num7)
+
+// ---------------#6Qb97gsv------------
+
+// let firstName = 'Artem'
+// let middleName = 'Boshkow'
+// let lastName = 'Doljnenko'
+// let person = `${lastName} ${firstName} ${middleName}`
+// console.log(person)
+
+
+// --------------------------#4N0y5tufA-----------------
+
+// let a = 100;
+// let b = '100'
+// let c = true;
+//
+// console.log(a, typeof a)
+// console.log(b, typeof b)
+// console.log(c, typeof c)
+
+// -----------------#ruUtWDUI--------------
+
+// let firstName = prompt("What is your first name?");
+// let lastName = prompt("What is your last name?");
+// let age = +prompt("What is your age number?");
+//
+// let person = `${firstName} ${lastName} ${age}`;
+// console.log(person);
