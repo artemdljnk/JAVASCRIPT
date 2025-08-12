1aef897a82d24cd9adc0864fc1bdbd26a9bb679e
ArtemDljnk <artemodessa31.01@gmail.com>
2025-08-12 21:07:07 +0300

    hw6

---
+++ 6/main.js
@@ -0,0 +1,53 @@
+// -----------------#dFeorS3m7u-----------
+
+// let text1 =  'hello world'
+// console.log(text1.length)
+// let text2 =  'lorem ipsum'
+// console.log(text2.length)
+// let text3 =  'js is cool'
+// console.log(text3.length)
+
+// --------------#8lld9HMxXWB-------------
+
+// let text1 =  'hello world'
+// console.log(text1.toUpperCase())
+// let text2 =  'lorem ipsum'
+// console.log(text2.toUpperCase())
+// let text3 =  'js is cool'
+// console.log(text3.toUpperCase())
+
+// ---------------------------#ClDsAm7xba7------------
+
+// let text1 = "HELLO WORLD"
+// console.log(text1.toLowerCase())
+// let text2 = 'LOREM IPSUM'
+// console.log(text2.toLowerCase())
+// let text3 = 'JS IS COOL'
+// console.log(text3.toLowerCase())
+
+// -------------------#0b89BkYZwu-------------
+
+// let str = `dirty string   `
+// console.log(str.trim())
+
+// ---------------#bfoJuse4ZzP-----------
+
+// let str = 'Ревуть воли як ясла повні';
+// console.log(str.split(` `))
+
+// ---------------#Rbr5kEQ---------------
+//
+// let arr = [10,8,-7,55,987,-1011,0,1050,0]
+// let str = arr.map(value => {
+//     return value + ''
+// })
+// console.log(str)
+
+// ------------------------#5hqyKTfmc----------
+
+function sortNums(arrayOfNumbers, direction){
+    if(direction === 'asc') return arrayOfNumbers.sort((a,b)=>a-b)
+    if(direction === 'desc') return arrayOfNumbers.sort((a,b)=>b-a)
+
+
+}
