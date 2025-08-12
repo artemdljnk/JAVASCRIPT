4d8b99fa5e05b46b6e0a78c7ae1e58b3ae439663
ArtemDljnk <artemodessa31.01@gmail.com>
2025-08-12 15:37:01 +0300

    hw3

---
+++ 3/main.js
@@ -0,0 +1,102 @@
+// --------------------#y7crMeFwHcS-------------------
+
+// for (let i = 1; i < 11; i++) {
+//     document.write('<div>Hello</div>')
+// }
+
+// ---------------#TYj7ncx---------------
+
+// for (let i = 1; i < 11; i++) {
+//     document.write(`<div>Hello ${i}</div>`)
+// }
+
+// -----------------#uzkt71dp--------------
+
+// let i = 1;
+// while (i < 21) {
+//     document.write(`<h1>Hello</h1>`)
+//     i++
+// }
+
+// ---------------#OeT7t3uUMFi------------
+
+// let i = 1;
+// while (i < 21) {
+//     document.write(`<h1>Hello< ${i}/h1>`)
+//     i++
+// }
+
+// ------------------#vLSZKMlO------------
+
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+// for (const listOfItem of listOfItems) {
+//     document.write(`<ul><li>${listOfItem}</li></ul>`)
+// }
+
+// -------------#Hdjws7E-----------
+
+// let products = [
+//     {
+//         title: 'milk',
+//         price: 22,
+//         image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300×300-3.jpg'
+//     },
+// {
+//         title: 'juice',
+//         price: 27,
+//         image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+// },
+// {
+//         title: 'tomato',
+//         price: 47,
+//         image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+// },
+// {
+//         title: 'tea',
+//         price: 15,
+//         image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+// },
+// ];
+//
+// for (const product of products) {
+//     document.write(`
+//     <div class=”product-card”>
+//         <h3 class=”product-title”>${product.title}. Price – ${product.price}</h3>
+//         <img src=”${product.image}” alt=”” class=”product-image”>
+//     </div>
+//     `)
+// }
+
+// ------------------#4WrHwFTEop0--------------
+
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+//
+// for (const user of users) {
+//     if (user.status) {
+//         document.write(`<div>${user.name}</div>`);
+//     }
+// }
+//
+// for (const user of users) {
+//     if (!user.status) {
+//         document.write(`<div>${user.name}</div>`);
+//     }
+// }
+//
+// for (const user of users) {
+//     if (user.age > 30){
+//         document.write(`<div>${user.name}</div>`);
+//     }
+// }
