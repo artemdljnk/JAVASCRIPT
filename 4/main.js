83619ec2a7372e70a017143093b719a5b997d0ca
ArtemDljnk <artemodessa31.01@gmail.com>
2025-08-12 18:36:20 +0300

    hw4

---
+++ 4/main.js
@@ -0,0 +1,57 @@
+// ----------------------#I2XsG6f---------------
+
+// function square(aNum, bNum) {
+//     const result = aNum * bNum;
+//     console.log(result);
+//     return result;
+// }
+//
+// square()
+
+// -----------#ETGAxbEn8l----------
+
+// function roundSquare(radius) {
+//     return Math.PI * radius * radius;
+// }
+
+// -----------------------#Mbiz5K4yFe7--------------
+
+// function squareCilinder(radius, height) {
+//     return 2 * Math.PI * radius * height
+// }
+//
+// console.log(squareCilinder(10, 40));
+
+// ------------------#SIdMd0hQ-------------
+
+// function foobar(array) {
+//     for (const item of array) {
+//         console.log(item);
+//     }
+// }
+
+// ------------------#59g0IsA---------------
+
+// function foobar(text) {
+//     document.write(`<p>${text}</p>`);
+// }
+//
+// foobar('okten');
+// foobar('okten1');
+// foobar('okten2');
+// foobar('okten3');
+
+// --------------------#hOL6126-----------------
+
+// function foobar(text) {
+//     document.write(`
+//                         <ul>
+//                             <li>${text}</li>
+//                             <li>${text}</li>
+//                             <li>${text}</li>
+//                         </ul>
+//                         `)
+//
+// }
+//
+// foobar('hello okten');
