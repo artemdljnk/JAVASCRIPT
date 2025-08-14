480e36cc539d43f0ff1eacb6bf0159ad32cfc651
ArtemDljnk <artemodessa31.01@gmail.com>
2025-08-14 17:17:50 +0300

    hw7

---
+++ 7/main.js
@@ -0,0 +1,120 @@
+// ----------------#XjJuucOMR0-----------
+
+// function User(id, name, username, phone){
+//     this.id = id;
+//     this.name = name;
+//     this.username = username;
+//     this.phone = phone;
+// }
+//
+// let users = [
+//     new User(1, 'koks', 'qwerty', '+1442423'),
+//     new User(2, 'koks', 'qwerty', '+1442423'),
+//     new User(3, 'koks', 'qwerty', '+1442423'),
+//     new User(4, 'koks', 'qwerty', '+1442423'),
+//     new User(5, 'koks', 'qwerty', '+1442423'),
+//     new User(6, 'koks', 'qwerty', '+1442423'),
+//     new User(7, 'koks', 'qwerty', '+1442423'),
+//     new User(8, 'koks', 'qwerty', '+1442423'),
+//     new User(9, 'koks', 'qwerty', '+1442423'),
+//     new User(10, 'koks', 'qwerty', '+1442423'),
+// ]
+// console.log(users);
+
+// -------------------#2ikXsE2WiKZ----------
+
+// function User(id, name, username, phone){
+//     this.id = id;
+//     this.name = name;
+//     this.username = username;
+//     this.phone = phone;
+// }
+// let users = [
+//     new User(1, 'koks', 'qwerty', '+1442423'),
+//     new User(2, 'koks', 'qwerty', '+1442423'),
+//     new User(3, 'koks', 'qwerty', '+1442423'),
+//     new User(4, 'koks', 'qwerty', '+1442423'),
+//     new User(5, 'koks', 'qwerty', '+1442423'),
+//     new User(6, 'koks', 'qwerty', '+1442423'),
+//     new User(7, 'koks', 'qwerty', '+1442423'),
+//     new User(8, 'koks', 'qwerty', '+1442423'),
+//     new User(9, 'koks', 'qwerty', '+1442423'),
+//     new User(10, 'koks', 'qwerty', '+1442423'),
+// ]
+//
+// const filtered = users.filter(user => user.id % 2 === 0)
+// console.log(filtered)
+
+// ---------------------#pOeHKct---------------
+
+// function User(id, name, username, phone){
+//     this.id = id;
+//     this.name = name;
+//     this.username = username;
+//     this.phone = phone;
+// }
+// let users = [
+//     new User(1, 'koks', 'qwerty', '+1442423'),
+//     new User(2, 'koks', 'qwerty', '+1442423'),
+//     new User(3, 'koks', 'qwerty', '+1442423'),
+//     new User(4, 'koks', 'qwerty', '+1442423'),
+//     new User(5, 'koks', 'qwerty', '+1442423'),
+//     new User(6, 'koks', 'qwerty', '+1442423'),
+//     new User(7, 'koks', 'qwerty', '+1442423'),
+//     new User(8, 'koks', 'qwerty', '+1442423'),
+//     new User(9, 'koks', 'qwerty', '+1442423'),
+//     new User(10, 'koks', 'qwerty', '+1442423'),
+// ]
+// users.sort((a, b) => b.id - a.id)
+// console.log(users);
+
+// ----------------#nkMXISv-------------
+
+// function Client(id, name, surname, email, phone, order){
+//     this.id = id;
+//     this.name = name;
+//     this.surname = surname;
+//     this.email = email;
+//     this.phone = phone;
+//     this.order = order;
+// }
+//
+// let clients = [
+//     new Client(1, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', 'dqfqfqf'),
+//     new Client(2, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', 'dqfqfqf'),
+//     new Client(3, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', 'dqfqfqf'),
+//     new Client(4, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', 'dqfqfqf'),
+//     new Client(5, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', 'dqfqfqf'),
+//     new Client(6, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', 'dqfqfqf'),
+//     new Client(7, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', 'dqfqfqf'),
+//     new Client(8, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', 'dqfqfqf'),
+//     new Client(9, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', 'dqfqfqf'),
+//     new Client(10, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', 'dqfqfqf'),
+// ]
+// console.log(clients);
+
+// ----------------#8abtVjRv---------------
+
+// function Client(id, name, surname, email, phone, order){
+//     this.id = id;
+//     this.name = name;
+//     this.surname = surname;
+//     this.email = email;
+//     this.phone = phone;
+//     this.order = order;
+// }
+//
+// let clients = [
+//     new Client(1, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', '10'),
+//     new Client(2, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', '9'),
+//     new Client(3, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', '8'),
+//     new Client(4, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', '7'),
+//     new Client(5, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', '6'),
+//     new Client(6, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', '5'),
+//     new Client(7, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', '4'),
+//     new Client(8, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', '3'),
+//     new Client(9, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', '2'),
+//     new Client(10, 'Artem', 'Dljnk', 'aaasddf@gmail.com','+12039', '1'),
+// ]
+// let sorted = clients.sort((a, b) => a.order - b.order);
+// console.log(sorted);
